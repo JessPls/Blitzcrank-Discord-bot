eb782c236b93cbae9264c0188c1e200e8087dc00
Joel Kophazi <jdkophazi@gmail.com>
2020-05-30 00:28:10 -0400

    Added author, condenced code (#13)

--- commands/dice.js
+++ commands/dice.js
@@ -1,8 +1,10 @@
 /**
  * The dice command will roll a standard 6 sided die, or a die with as many sides as the user wants.
  * This depends on whether the user gives a numerical parameter or not
+ * v1.0.2 - Now may roll multiple dice, up to a limit.
  * 
  * @author Jess Queen
+ * @author Joel Kophazi
  */
 module.exports = {
     name: "dice",
@@ -13,24 +15,17 @@ module.exports = {
         var loops = 1;
         var limit = 20;
         if (args[1] && parseInt(args[1])) {
-            sides = parseInt(args[1]);
-        }
+            sides = parseInt(args[1]);}
         if (args[2] && parseInt(args[2])) {
-            loops = parseInt(args[2]);
-        }
+            loops = parseInt(args[2]);}
         if (loops <= limit) {
             for (round = 0; round < loops; round++) {
-                var n = n + Math.floor(Math.random() * sides + 1);
-            }
+                var n = n + Math.floor(Math.random() * sides + 1);}
             if (loops === 1) {
-                msg.reply("you rolled a " + n + "!");
-            }
+                msg.reply("you rolled a " + n + "!");}
             else {
-                msg.reply("you rolled " + loops + " dice for a total of " + n + "!");
-            }
-        }
+                msg.reply("you rolled " + loops + " dice for a total of " + n + "!");}}
         else {
-            msg.reply("you rolled too many dice (limit = " + limit + ")");
-        }
+            msg.reply("you rolled too many dice (limit = " + limit + ")");}
     }
 }
